5431183112847734345b73ce19a2dc4e403a95dd
Vladimir Bautista Diaz <vbautista@caracol.com.co>
2025-07-22 16:50:54 -0500

    Actualización de perfil.py

--- backend/data_loader/perfil.py
+++ backend/data_loader/perfil.py
@@ -18,7 +18,46 @@ def load_saved_value():
             try:
                 saved_value_perfil = saved_df['Perfil'].iloc[0]
                 st.session_state['profile'] = saved_value_perfil
+                
+                saved_value = saved_df['Valor Looker'].iloc[0]
+                saved_value_Pas = saved_df['Valor Read Pas'].iloc[0]
+                saved_value_Pds = saved_df['Valor Read Pds'].iloc[0]
+                saved_value_table = saved_df['Valor Table PMPs'].iloc[0]
+                saved_value_export_ctr = saved_df['Valor Export CTR'].iloc[0]
+                saved_value_export_vw = saved_df['Valor Export VW'].iloc[0]
+                saved_value_export_cr = saved_df['Valor Export CR'].iloc[0]
+                saved_value_file_ias = saved_df['Path IAS'].iloc[0]
+                saved_value_file_gam = saved_df['Path GAM'].iloc[0]
+                saved_value_file_dcm = saved_df['Path DCM'].iloc[0]
+                saved_value_file_dv = saved_df['Path DV'].iloc[0]
+                saved_value_file_abk = saved_df['Path ABK'].iloc[0]
+                saved_value_file_ttd = saved_df['Path TTD'].iloc[0]
+                saved_value_file_tap = saved_df['Path TAP'].iloc[0]
+                saved_value_looker = saved_df['Valor Looker Consolidado'].iloc[0]
+                saved_value_OneDrive_Direct = saved_df['Path OneDrive Directas'].iloc[0]
+                saved_value_file_xandr = saved_df['Xandr Path'].iloc[0]
+                
+                st.session_state['looker_pds'] = saved_value
+                st.session_state['file_pas'] = saved_value_Pas
+                st.session_state['file_pds'] = saved_value_Pds
+                st.session_state['file_table_pds'] = saved_value_table
+                st.session_state['file_export_ctr'] = saved_value_export_ctr
+                st.session_state['file_export_vw'] = saved_value_export_vw
+                st.session_state['file_export_cr'] = saved_value_export_cr
+                st.session_state['path_ias'] = saved_value_file_ias
+                st.session_state['path_gam'] = saved_value_file_gam
+                st.session_state['path_dcm'] = saved_value_file_dcm
+                st.session_state['path_dv'] = saved_value_file_dv
+                st.session_state['path_adbook'] = saved_value_file_abk
+                st.session_state['path_ttd'] = saved_value_file_ttd
+                st.session_state['path_tap'] = saved_value_file_tap
+                st.session_state['path_one_drive'] = saved_value_OneDrive_Direct
+                st.session_state['path_xandr'] = saved_value_file_xandr
+                st.session_state['looker_consolidado'] = saved_value_looker 
+                
+                
                 st.write("✅ Perfil cargado:", saved_value_perfil)
+                st.write(saved_value_file_ias)
             except Exception as e:
                 st.error(f"❌ Error al cargar perfil: {e}")              
         '''if os.path.exists('./frontend/assets/perfil.xlsx'):
